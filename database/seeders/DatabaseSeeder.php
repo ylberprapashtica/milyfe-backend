3e401ae4274c0edf80a0eef80d7bb270188ca093
Ylber Prapashtica <ylberprapashtica@gmail.com>
2026-02-03 01:27:14 +0100

    Add CaptureStatusSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CaptureTypeSeeder::class,
+            CaptureStatusSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
